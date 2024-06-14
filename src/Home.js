4088f68a9a6faba2ef49475b94840593d94da4a9
ace <19754841+aceinthedeck@users.noreply.github.com>
2024-06-14 09:08:43 -0400

    addded state hook

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { createRoot } from 'react-dom/client';
 
 function Home() {
-    return (
-        <div>
-            This is home page component on github pages
-            <Link to={"/contact"}>Contact Link</Link>
-        </div>
-    );
-}
 
+    const [counter, setCounter] = useState(0);
+
+    return(
+        <div>
+            <button onClick={() => setCounter(counter+1)}>Increase value</button>
+            <div>Counter value is {counter}</div>
+        </div>
+    )
+}
 export default Home;
